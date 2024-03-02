02815ca23a497872839290042d780aded9476fbc
Alejandro Rozas <ecosistemas10@cnp.com.ar>
2024-03-02 14:16:57 -0300

    Tipos de datos y typeof

--- universidad-javascript/variables/nombres-variables.js
+++ universidad-javascript/variables/nombres-variables.js
@@ -1,4 +1,17 @@
-//Declarando con signo $.
+//Declarando con signo dolar $.
 
 let $miNumero1 = 10;
 console.log($miNumero1);
+
+//Declarando con guión bajo
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+//Esta notación en Camel Case es la buena practica al declarar variables en JS.
+
+let miNumero3 = 30;
+console.log(miNumero3)
+
+/* JavaScript es sensible a mayusculas y minusculas */
+//console.log(MiNumero3)
